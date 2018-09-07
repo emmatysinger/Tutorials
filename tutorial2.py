42e47f5a3ba2f4abe568f6f3c8c6473fb9e15d9a
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-09-07 11:44:22 -0400

    Updated from Brython Server: 9/7/2018 11:44:18 AM

--- tutorial2.py
+++ tutorial2.py
@@ -1,1 +1,19 @@
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
 
+red = Color(0xff0000, 0.5)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 0.5)
+black = Color(0x000000, 0.5)
+
+thinline = LineStyle(1, black)
+mycircle1 = CircleAsset(5, thinline, blue)
+mycircle2 = CircleAsset(5, thinline, red)
+xcoordinates = range(100, 600, 10)
+
+# Generate a list of sprites that form a line!
+sprites = [Sprite(mycircle1, (x, x*0.5 + 100)) for x in range(100, 600, 10)]
+sprites = [Sprite(mycircle2, (x, x*0.5 +100)) for x in range(100, 600, 10)]
+
+myapp = App()
+myapp.run()
