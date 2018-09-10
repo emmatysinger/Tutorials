b2c27b1ef393816549996ac1656d5d2dda864749
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-09-10 11:48:33 -0400

    Updated from Brython Server: 9/10/2018 11:48:29 AM

--- tutorial3.py
+++ tutorial3.py
@@ -1,1 +1,56 @@
+#temperature = float(input("What is the temperature outside? "))
+#weather = input("Is it raining or sunny outside? ")
+#weather = weather.lower() # change to lower case
+#if temperature < 55:
+    #clothes = "a warm raincoat" if weather == "raining" else "a fleece"
+#else:  # 55 or warmer..
+    #clothes = "an umbrella" if weather == "raining" else "sunglasses"
+#print("You should bring {0}.".format(clothes))
+#temperatures =[12,46,24,67,80,26,64,24,45,40]
+#howitfeels=["{0} is Brr!".format(temp) if temp < 35 else temp for temp in temperatures]
+#print (howitfeels)
 
+a=1
+if a==1:
+    print("True")
+    
+a = 3
+b = 55
+if a == 3 and b == 4:
+    print("yes")
+else:
+    print("no")
+    
+a = 3
+b = 55
+if a == 3 or b == 4:
+    print("yes")
+else:
+    print("no")
+
+3==3
+3!=3
+3>=4
+not(3<4)
+
+a=5
+b=4
+if a<b:
+    print("True")
+else:
+    print("False")
+
+a=5
+b=4
+if a!=b:
+    print("True")
+else:
+    print("False")
+    
+a=5
+b=4
+c=3
+if a<b and c!=3:
+    print("True")
+else:
+    print("False")
