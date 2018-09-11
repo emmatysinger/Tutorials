43faf01d7ef45f4d9ca912c4221068e2cb94b674
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-09-11 11:36:07 -0400

    Updated from Brython Server: 9/11/2018 11:35:57 AM

--- tutorial4.py
+++ tutorial4.py
@@ -1,1 +1,28 @@
-
+dinnercost = 15     # cost of dining out
+bankaccount = 150   # dollars
+while bankaccount > 0:
+    print("Yumm.. I would love to eat out tonight!")
+    bankaccount = bankaccount - dinnercost
+    print("Uh oh.. I only have ${0} left!".format(bankaccount))
+    
+for i in range(5):
+    print(i)
+    
+for i in ['a', 'b', 'c', 'd']:
+    print(i)
+    
+for i in [1, 2, 3]:
+    for j in [4, 5]:
+        print(i, j)
+        
+b = -4
+while b <= 0:
+    b += 1
+    print(b)
+    
+for i in [1, 2, 3]:
+    for j in range(i):
+        print(i, j)
+        
+while 1 == 99:
+    print("hi")
